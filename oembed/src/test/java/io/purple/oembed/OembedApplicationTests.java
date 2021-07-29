a0b67a57103faba41ec04d3655c975be59ad608b
juhwano <calculus9006@gmail.com>
2021-07-29 15:25:32 +0900

    🎉프로젝트 시작

---
+++ oembed/src/test/java/io/purple/oembed/OembedApplicationTests.java
@@ -0,0 +1,13 @@
+package io.purple.oembed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OembedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
